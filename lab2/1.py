558ba98cd8195289bfd40c16c9c5577e8f3450a2
Asagynbek0 <alinursagynbek00@gmail.com>
2025-01-22 11:35:14 +0500

    update all labs

---
+++ lab2/1.py
@@ -0,0 +1,12 @@
+#Python Booleans
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+  
+#Python Operators
+print((6 + 3) - (6 + 3))
+print(100 + 5 * 3)
