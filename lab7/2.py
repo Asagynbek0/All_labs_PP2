50568428c2ad314a51499828e547c8747c8dc3e4
Asagynbek0 <alinursagynbek00@gmail.com>
2025-03-24 15:55:38 +0500

    lab7

---
+++ lab7/2.py
@@ -0,0 +1,43 @@
+import pygame
+
+playlist = [r"C:\Users\alinu\Documents\new_folder\labs\Wake Up In The Sky.mp3"]
+current_track = 0
+
+def play():
+  pygame.mixer.music.load(playlist[current_track])
+  pygame.mixer.music.play(-1)
+  print(f"Playing")
+  
+def stop():
+  pygame.mixer.music.stop()
+  print("Music stopped")
+
+def next():
+  global current_track
+  current_track = (current_track + 1) % len(playlist)
+  play()
+
+def previous():
+  global current_track
+  current_track = (current_track - 1) % len(playlist)
+  play()
+
+pygame.init()
+done = False
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((200, 200))
+
+while not done:
+  clock.tick(60)
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      done = True
+
+  screen.fill((255, 255, 255))
+  pressed = pygame.key.get_pressed() 
+  if pressed[pygame.K_SPACE]: stop() #stop
+  if pressed[pygame.K_0]: play() #play
+  if pressed[pygame.K_RIGHT]: next() #next
+  if pressed[pygame.K_LEFT]: previous() #previous
+
+  pygame.display.flip()
